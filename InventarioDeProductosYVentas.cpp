c15e9b1f7abb40eef9bff3c3effee63a9b48cb24
Gabriel <gpenalozag@unjbg.edu.pe>
2024-07-09 13:37:50 -0500

    Añadiendo la función principal con sus respectivas opciones

--- InventarioDeProductosYVentas.cpp
+++ InventarioDeProductosYVentas.cpp
@@ -16,3 +16,52 @@ struct Venta {
     int cantidad;
     float precioTotal;
 };
+
+int main() {
+    SetConsoleOutputCP(CP_UTF8);
+    Producto productos[MAX_PRODUCTOS];
+    Venta ventas[MAX_PRODUCTOS];
+    int numProductos = 0;
+    int contadorVentas = 0;
+    char opcion;
+
+    do {
+        cout << "a) Registrar producto\nb) Listar productos\nc) Buscar producto\nd) Actualizar producto\ne) Eliminar producto\nf) Registrar venta\ng) Listar ventas\nh) Calcular total de ventas\ns) Salir\n";
+        cout << "Seleccione una opción: ";
+        cin >> opcion;
+
+        switch (opcion) {
+            case 'a':
+                registrarProducto(productos, numProductos);
+                break;
+			case 'b':
+                listarProductos(productos, numProductos);
+                break;
+            case 'c':
+                buscarProducto(productos, numProductos);
+                break;
+            case 'd':
+                actualizarProducto(productos, numProductos);
+                break;
+            case 'e':
+                eliminarProducto(productos, numProductos);
+                break;
+            case 'f':
+                registrarVenta(ventas, productos, contadorVentas, numProductos);
+                break;
+            case 'g':
+                listarVentas(ventas, contadorVentas);
+                break;
+            case 'h':
+                calcularTotalVentas(ventas, contadorVentas);
+                break;
+            case 's':
+                cout << "Saliendo del programa...\n";
+                break;
+            default:
+                cout << "Opción no válida.\n";
+        }
+    } while (opcion != 's');
+
+    return 0;
+}
